fd574ad1b6b9d53b94d0e967c881a806509eaf57
Blake Don <randong284@gmail.com>
2015-10-22 16:36:51 -0400

    this afternoon work

---
+++ MaximalSubarray.java
@@ -0,0 +1,9 @@
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @return: A integer indicate the sum of max subarray
+     */
+    public int maxSubArray(int[] nums) {
+        // write your code
+    }
+}
--- MinimumSubarray.java
+++ MinimumSubarray.java
@@ -4,7 +4,26 @@ public class Solution {
      * @return: A integer indicate the sum of minimum subarray
      */
     public int minSubArray(ArrayList<Integer> nums) {
-        // write your code
+        int globalMinSum = nums.get(0);
+        int sum = nums.get(0);
+        
+        for(int i = 1; i < nums.size(); i++) {
+            
+            int temp = nums.get(i);
+            
+            if(sum > 0) {
+                sum = temp;
+                globalMinSum = Math.min(globalMinSum, sum);
+            } else {
+                sum += temp;
+                if(sum <= 0) {
+                    globalMinSum = Math.min(globalMinSum, sum);
+                } else {
+                    sum = temp;
+                }
+            }
+            
+        }
+        return globalMinSum;
     }
 }
-
