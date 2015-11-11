aceb5d98cdcee738578ab3757a9319ff2d01c80c
Blake Don <randong284@gmail.com>
2015-11-11 17:39:42 -0500

    hi

--- NumberOfIslands.java
+++ NumberOfIslands.java
@@ -12,7 +12,7 @@ public class Solution {
             return 0;
         }
 
-        int count = 0;
+        int count = 0; //
 
         for(int i = 0; i < grid.length; i++) {
             for(int j = 0; j < grid[0].length; j++) {
